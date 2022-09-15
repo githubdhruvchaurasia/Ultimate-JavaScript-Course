e52b39b17d0bc20f554f2c3e6460cb5886b9f68f
Dhruv Chaurasia <94190323+githubdhruvchaurasia@users.noreply.github.com>
2022-09-15 23:12:09 +0530

    Add files via upload

---
+++ 05. Chapter_1 Practise Set.js
@@ -0,0 +1,11 @@
+
+// Question - 1
+let a = "Dhruv Chaurasia"
+let b = 76
+console.log(a + b)
+
+
+// Question - 2
+console.log(typeof a);
+console.log(typeof b);
+
---
+++ 06. Operataors & Expressions.js
@@ -0,0 +1,56 @@
+
+// // Arithmetic Operators
+
+let a = 15
+let b = 5
+
+console.log("a + b = ", a + b) = 20
+console.log("a - b = ", a - b) = 10
+console.log("a / b = ", a / b) = 3
+console.log("a * b = ", a * b) = 75
+console.log("a ** b = ", a ** b) = 759375
+console.log("a++ = ", a++) = 15
+console.log("++a = ", ++a) = 17
+console.log("b-- = ", b--) = 5
+console.log("--b = ", --b) = 3
+
+
+
+
+// // Assignment Operator 
+
+let assignment = 1;
+assignment += 5
+console.log(assignment)
+
+
+
+
+// Comparison operators
+
+let a = 5
+let b = 5
+
+console.log("a == b", a == b)
+console.log("a != b", a != b)
+console.log("a === b", a === b)
+console.log("a !== b", a !== b)
+
+// // Difference Between == and ===
+
+let aa = 5
+let bb = "5"
+
+console.log("aa == bb", aa == bb)
+console.log("aa != bb", aa != bb)
+console.log("aa === bb", aa === bb)
+console.log("aa !== bb", aa !== bb)
+
+
+
+
+// Logical Operators
+
+let x = 5;
+let y = 3;
+console.log(x < y && x == 5)
